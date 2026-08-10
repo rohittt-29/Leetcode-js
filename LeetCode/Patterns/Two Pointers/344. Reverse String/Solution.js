8bc840efa0eed4098718989a5b1558cb465ac49f
rohittt-29 <rm2193352@gmail.com>
2026-08-10 18:21:56 +0530

    Add JavaScript Solution to Patterns/Two Pointers

---
+++ LeetCode/Patterns/Two Pointers/344. Reverse String/Solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let len = s.length;
+    let half = Math.floor(len/2);
+    for(let i = 0; i<half; i++){
+        let temp = s[i];
+        s[i]= s[len-1-i];
+        s[len-1-i] = temp
+    }
+};
